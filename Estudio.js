fe164110e9df4cc61bba91be94311780ce749677
Auricel <148658628+Auricel@users.noreply.github.com>
2024-07-12 20:47:33 +0000

    inicio

---
+++ Estudio.js
@@ -0,0 +1,12 @@
+//                              metodos array
+
+//¿Cómo declarar un array?
+let myArray = []; // Lista vacia
+let myArray = ["Apple", "Orange", "Donkey"]; // Con 3 elementos tipo string
+let myArray = new Array(1,2,3,4,5); // ¡No uses esto! Lee a continuación para saber por qué
+
+//Añadiendo elementos (función push)
+let myArray = ['Pedro', 'Juan', 'Maria'];
+    myArray.push('Chris');
+    console.log(myArray); // Esto imprimirá ['Pedro', 'Juan', 'Maria', 'Chris'];
+    
